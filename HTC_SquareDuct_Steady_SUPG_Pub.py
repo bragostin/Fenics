a12dd58c5e4908e177c93696531353b921fdf58a
Bruno Agostini <bruno.agostini@gmail.com>
2020-10-07 13:04:42 +0200

    Update HTC_SquareDuct_Steady_SUPG_Pub.py

--- HTC_SquareDuct_Steady_SUPG_Pub.py
+++ HTC_SquareDuct_Steady_SUPG_Pub.py
@@ -1,3 +1,11 @@
+"""
+Fenics Incompressible Navier-Stokes
+This script solves the laminar flow with heat transfer problem in a square duct.
+It has SUPG / PSPG / LSIC stabilization implemented. 
+It yields a Nusselt number of 4.88 compared to 7.1 in Shah & London, Laminar Flow Forced Convection in Ducts, 1978. 
+This difference probably comes from the meshing of the square duct: a better meshing at the wall where the temperature gradient is calculated is probably needed.
+"""
+
 from dolfin import *
 from msh2xdmf import import_mesh_from_xdmf, msh2xdmf
 from ufl import Min
